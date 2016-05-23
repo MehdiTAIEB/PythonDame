a55009ffdda226738b30e97cb515db3a2baaee4e
taieb_a <taieb_a@epitech.eu>
2016-05-23 17:39:06 +0200

    setup rendering sprites

--- test.py
+++ test.py
@@ -3,7 +3,7 @@
 import os, sys
 import pygame
 from pygame.locals import *
-from sprites.basemap import Map
+from sprites.basemap import Builder
 
 if not pygame.font: print 'Warning, fonts disabled'
 if not pygame.mixer: print 'Warning, sound disabled'
@@ -11,7 +11,7 @@ if not pygame.mixer: print 'Warning, sound disabled'
 class Game:
     def __init__(self):
         pygame.init()
-        screen = pygame.display.set_mode((468, 60))
+        screen = pygame.display.set_mode((900, 600))
         pygame.display.set_caption('Jeu de Dame')
         # all the miscs Map()
         background = pygame.Surface(screen.get_size())
@@ -22,6 +22,8 @@ class Game:
         pygame.display.flip()
 
         clock = pygame.time.Clock()
+        builder = Builder()
+        sprites = pygame.sprite.RenderPlain((builder))
 
         while 1:
             clock.tick(60)
@@ -31,32 +33,9 @@ class Game:
                     return
                 elif event.type == KEYDOWN and event.key == K_ESCAPE:
                     return
-
-    def load_image(name, color=None):
-        fullname = os.path.join('misc', name)
-        try:
-            image = pygame.image.load(fullname)
-        except pygame.error, message:
-            print 'Cannot load image:', fullname
-            raise SystemExit, message
-        image = image.convert()
-        if colorkey is not None:
-            if color is -1:
-                color = image.get_at((0,0))
-            image.set_colorkey(color, RLEACCEL)
-        return image, image.get_rect()
-
-    def load_sound(name):
-        class NoneSound:
-            def play(self): pass
-        if not pygame.mixer or not pygame.mixer.get_init():
-            return NoneSound()
-        fullname = os.path.join('misc', name)
-        try:
-            sound = pygame.mixer.Sound(fullname)
-        except pygame.error, message:
-            print 'Cannot load sound:', fullname
-            raise SystemExit, message
-        return sound
+            sprites.update()
+            screen.blit(background, (0, 0))
+            sprites.draw(screen)
+            pygame.display.flip()
 
 Game();
