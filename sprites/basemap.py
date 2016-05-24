53fd76af59a871a98ca1d1ff003626de008cd8da
taieb_a <taieb_a@epitech.eu>
2016-05-24 18:38:01 +0200

    dcomment

--- sprites/basemap.py
+++ sprites/basemap.py
@@ -17,7 +17,7 @@ class Builder(pygame.sprite.Sprite):
         self.isDame = isDame
 
     def update(self):
-        if self.isPawn == True: # and if color is not already
+        if self.isPawn == True: # and if color is not already maybe correct grapic bug to over redraw
            self.image.fill((0,0,0,0))
            self.rect = self.image.get_rect()
         return
