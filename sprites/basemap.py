f6b07cbbaa60f8ce55ba2ba73ee557eb4425bbd4
taieb_a <taieb_a@epitech.eu>
2016-05-25 12:40:54 +0200

    push in array and be able to get object in dictionary to interact with

--- sprites/basemap.py
+++ sprites/basemap.py
@@ -3,17 +3,15 @@ import pygame
 from pygame.locals import *
 
 class Builder(pygame.sprite.Sprite):
-    def __init__(self, x, y, color, isPawn=False, isDame=False):
+    def __init__(self, x, y, color, isPawn=False, isDame=False, pOne=False, pTwo=False):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface([40, 40])
         self.x = x
         self.y = y
         self.isPawn = isPawn
         self.isDame = isDame
-        if self.isPawn == True:
-            self.updateColor((255, 255, 255))
-        else:
-            self.updateColor(color)
+        self.determinePlayer(pOne, pTwo)
+        self.setColor(color)
         self.image.fill(self.color)
         self.rect = self.image.get_rect()
         self.rect.x = x
@@ -21,5 +19,23 @@ class Builder(pygame.sprite.Sprite):
 
     def update(self):
         return
+
     def updateColor(self, color):
         self.color = color
+        self.image.fill(self.color)
+        self.rect = self.image.get_rect()
+
+    def determinePlayer(self, pOne, pTwo):
+        if pOne == True:
+            self.player = '1'
+        elif pTwo == True:
+            self.player = '2'
+
+    def setColor(self, color):
+        if self.isPawn == True:
+            if self.player == '1':
+                self.color = (255, 255, 255)
+            else:
+                self.color = (0, 0, 0)
+        else:
+            self.color = color
--- test.py
+++ test.py
@@ -11,6 +11,7 @@ if not pygame.mixer: print 'Warning, sound disabled'
 class Game:
     def __init__(self):
         pygame.init()
+        self.speList = {}
         screen = pygame.display.set_mode((400, 400))
         pygame.display.set_caption('Jeu de Dame')
         background = pygame.Surface(screen.get_size())
@@ -29,7 +30,8 @@ class Game:
             for event in pygame.event.get():
                 if event.type == QUIT:
                     return
-                elif event.type == KEYDOWN and event.key == K_ESCAPE:
+                elif event.type == MOUSEBUTTONDOWN:
+                    print('')
                     return
             sprites.update()
             screen.blit(background, (0, 0))
@@ -39,6 +41,8 @@ class Game:
     def mapFactory(self):
         isDame = False
         isPawn = False
+        pOne = False
+        pTwo = False
         ci = False
         i = 0
         offset = 0
@@ -47,19 +51,7 @@ class Game:
         building = 1
 
         while building:
-            cic = i + 1
-#functionize it
-            if cic % 2 != 0 and i < 9:
-                isPawn = True # setup color here or form
-            elif i > 9 and i < 20 and i % 2 != 0:
-                isPawn = True
-            elif i >= 20 and i < 30 and i % 2 == 0:
-                isPawn = True
-            elif i > 30 and i < 40 and i % 2 != 0:
-                isPawn = True
-            else:
-                isPawn = False
-#that too
+            isPawn, pOne, pTwo = self.determineIfPawn(i)
             if i % 10 == 0 and i != 0:
                 if ci == False:
                     ci = True
@@ -67,21 +59,82 @@ class Game:
                     ci = False
                 offset = 0
                 height = height - 40
-#and that lol
+
             if i <= 100:
                 if ci == True:
                     color = self.defineColor(i + 1)
                 else:
                     color = self.defineColor(i)
-                sp.append(Builder(offset, height, color, isPawn, isDame))
-                #push in dictionary to get repere
+                row = self.storeSprites(offset, height, color, isPawn, isDame, pOne, pTwo, i)
+                sp.append(self.speList[row + str(i)])
                 offset = offset + 40
                 i = i + 1
             else:
                 building = 0
-#then
+        self.speList['a0'].updateColor((0,0,0))
         sprites = pygame.sprite.RenderPlain(sp)
         return sprites
+
+    def defineRow(self, i):
+        row = ''
+        if i < 10:
+            row = 'a'
+        elif i < 20:
+            row = 'b'
+        elif i < 30:
+            row = 'c'
+        elif i < 40:
+            row = 'd'
+        elif i < 50:
+            row = 'e'
+        elif i < 60:
+            row = 'f'
+        elif i < 70:
+            row = 'g'
+        elif i < 80:
+            row = 'h'
+        elif i < 90:
+            row = 'i'
+        elif i < 100:
+            row = 'j'
+        return row
+
+    def storeSprites(self, offset, height, color, isPawn, isDame, pOne, pTwo, i):
+        row = self.defineRow(i)
+        self.speList[row + str(i)] = Builder(offset, height, color, isPawn, isDame, pOne, pTwo)
+        return row
+
+    def determineIfPawn(self, i):
+        pOne = False
+        pTwo = False
+        cic = i + 1
+        if cic % 2 != 0 and i < 9:
+            isPawn = True # setup color here or form
+            pOne = True
+        elif i > 9 and i < 20 and i % 2 != 0:
+            isPawn = True
+            pOne = True
+        elif i >= 20 and i < 30 and i % 2 == 0:
+            isPawn = True
+            pOne = True
+        elif i > 30 and i < 40 and i % 2 != 0:
+            isPawn = True
+            pOne = True
+        elif i >= 60 and i < 70 and i % 2 == 0:
+            isPawn = True
+            pTwo = True
+        elif i > 70 and i < 80 and i % 2 != 0:
+            isPawn = True
+            pTwo = True
+        elif i >= 80 and i < 90 and i % 2 == 0:
+            isPawn = True
+            pTwo = True
+        elif i > 90 and i < 100 and i % 2 != 0:
+            isPawn = True
+            pTwo = True
+        else:
+            isPawn = False
+        return isPawn, pOne, pTwo
 
     def defineColor(self, number):
         if (number % 2 == 0):
